b417da2489748b457fefa59790e275aba76b00d2
August2008 <davidlars99@gmail.com>
2013-04-11 21:30:35 -0400

    Test 2

--- August2008/Controllers/HomeController.cs
+++ August2008/Controllers/HomeController.cs
@@ -17,7 +17,7 @@ namespace August2008.Controllers
 
         public ActionResult About() 
         {
-            ViewBag.Message = "Your app description page.";
+            ViewBag.Message = "Your app description page...";
 
             return View();
         }
