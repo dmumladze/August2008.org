839ef55ba7664e81b5a6395d26225fc2c1d88bd4
August2008 <davidlars99@gmail.com>
2013-05-12 19:26:36 -0400

    Started hero related work..

---
+++ August2008/Models/Hero.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace August2008.Models
+{
+    public class Hero
+    {
+        public int HeroId { get; set; }
+        public int MilitaryRankId { get; set; }
+        public int MilitaryGroupId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Biography { get; set; }
+        public DateTime DateUpdated { get; set; }
+        public int UpdatedBy { get; set; }
+        public int LanguageId { get; set; }
+    }
+}
