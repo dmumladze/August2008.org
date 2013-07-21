e9a774d145fb6dce00e03051777e0827aabb474b
simonmed <simon6262000@yahoo.com>
2013-07-21 15:02:48 -0400

    Created SearchDonation stored procedure

---
+++ August2008Db/dbo/Stored Procedures/SearchDonations.sql
@@ -0,0 +1,18 @@
+﻿create PROC dbo.SearchDonations
+	@StartDate		DATETIME,
+	@EndDate		DATETIME
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	SELECT 
+		 d.DonationId
+		,u.DisplayName
+		,d.Amount
+		,d.Currency
+		,d.UserMessage
+		,d.DateDonated				
+	FROM dbo.Donation d (NOLOCK) 
+	INNER JOIN dbo.[User] u (NOLOCK) ON d.UserId = u.UserId
+	WHERE d.DateDonated BETWEEN @StartDate AND @EndDate
+END;
