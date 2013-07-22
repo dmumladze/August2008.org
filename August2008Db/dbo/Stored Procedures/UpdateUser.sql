b76dd9a4ac241dc5c31715dc4da7f23efb771460
simonmed <simon6262000@yahoo.com>
2013-07-22 19:45:19 -0400

    2 SPROC WAS CREATED

---
+++ August2008Db/dbo/Stored Procedures/UpdateUser.sql
@@ -0,0 +1,15 @@
+﻿CREATE PROC dbo.UpdateUser
+	@UserId			INT,
+	@Email			NVARCHAR(50) = NULL,
+	@DisplayName	NVARCHAR(50) = NULL
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	UPDATE dbo.[User]
+	SET Email	    = ISNULL(@Email, Email),
+		DisplayName = ISNULL(@DisplayName, DisplayName)
+	WHERE UserId = @UserId
+	AND (@Email IS NOT NULL OR 
+		 @DisplayName IS NOT NULL); 
+END;
---
+++ August2008Db/dbo/Stored Procedures/UpdateUserProfile.sql
@@ -0,0 +1,17 @@
+﻿CREATE PROC dbo.UpdateUserProfile
+	@UserId			INT,
+	@LanguageId		INT,
+	@Dob			DATETIME	 = NULL,
+	@Nationality	NVARCHAR(50) = NULL
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	UPDATE dbo.UserProfile
+	SET	LanguageId = @LanguageId,
+		Dob		   = ISNULL(@Dob,Dob),
+		Nationality= ISNULL(@Nationality,Nationality)
+	WHERE UserId = @UserId AND
+		(@Dob IS NOT NULL OR
+		 @Nationality IS NOT NULL);
+END
