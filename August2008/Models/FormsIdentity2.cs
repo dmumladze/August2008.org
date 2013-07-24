2e8132cfdf2a5c7a0eafecff342321750ae21b49
August2008 <davidlars99@gmail.com>
2013-07-24 12:57:56 -0400

    merge commit

---
+++ August2008/Models/FormsIdentity2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Web;
+using System.Web.Security;
+
+namespace August2008.Models
+{
+    public class FormsIdentity2 : IIdentity
+    {
+        public FormsIdentity2(string name, bool isAuthenticated = false)
+        {
+            Name = name;
+            IsAuthenticated = isAuthenticated;
+        }
+        public string AuthenticationType
+        {
+            get { return "Forms Authentication"; }
+        }
+        public bool IsAuthenticated { get; private set; }
+        public string Name { get; private set; }
+    }
+}
