fb24706b7c0396ad67cc32f95c8d82ef96b41dd9
August2008 <davidlars99@gmail.com>
2013-07-25 00:23:41 -0400

    Windows azure cloud services

---
+++ August2008.Common/CloudDataAccess.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using August2008.Common.Interfaces;
+using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace August2008.Common
+{
+    public class CloudDataAccess
+    {
+        private CloudStorageAccount _storageAccount;
+
+        public CloudDataAccess()
+            : this(ConfigurationManager.ConnectionStrings["August2008Azure"].ConnectionString)
+        {              
+        }
+        public CloudDataAccess(string connectionString)
+        {
+            _storageAccount = CloudStorageAccount.Parse(connectionString);          
+        }
+        public void AddBlob(string container, string blobName, Stream stream)
+        {
+            var blobClient = _storageAccount.CreateCloudBlobClient();
+            var container = blobClient.GetContainerReference(container);
+            container.CreateIfNotExists();
+            container.SetPermissions(new BlobContainerPermissions { PublicAccess = BlobContainerPublicAccessType.Blob });
+            var blobBlock = container.GetBlockBlobReference(blobName);
+            blobBlock.UploadFromStream(stream);
+        }
+
+    }
+}
