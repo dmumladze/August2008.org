f2988ee5a25a82516e758c814841e8c97b3ed36a
August2008 <davidlars99@gmail.com>
2013-07-27 21:59:32 -0400

    commit before merge

---
+++ August2008.Tests/Data/HeroRepositoryTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using August2008.Data;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace August2008.Tests.Data
+{
+    [TestClass]
+    public class HeroRepositoryTest
+    {
+        [TestMethod]
+        public void HeroDataClient_Can_Get_MilitaryRanks()
+        {
+        }
+    }
+}
