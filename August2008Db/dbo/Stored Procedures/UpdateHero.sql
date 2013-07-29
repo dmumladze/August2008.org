b6db539fb738f252f3be8b8cab7864f14d818564
August2008 <davidlars99@gmail.com>
2013-07-29 17:03:10 -0400

    UpdateHero

--- August2008Db/dbo/Stored Procedures/UpdateHero.sql
+++ August2008Db/dbo/Stored Procedures/UpdateHero.sql
@@ -38,3 +38,44 @@ BEGIN
 	AND LanguageId	= @LanguageId;
 END;
 
+CREATE PROCEDURE [dbo].[UpdateHero]
+	@HeroId				INT,
+	@LanguageId			INT,
+	@FirstName			NVARCHAR(50),
+	@LastName			NVARCHAR(75),
+	@MilitaryGroupId	INT				= NULL,
+	@MilitaryRankId		INT				= NULL,
+	@MilitaryAwardId	INT				= NULL,
+	@Dob				DATETIME		= NULL,
+	@Died				DATETIME		= NULL,	
+	@MiddleName			NVARCHAR(50)	= NULL,
+	@Biography			NVARCHAR(MAX)	= NULL,
+	@UpdatedBy			INT
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	UPDATE dbo.Hero 
+	SET
+		Dob				= ISNULL(@Dob, Dob),
+		Died			= ISNULL(@Died, Died),
+		UpdatedBy		= @UpdatedBy
+	WHERE 
+		HeroId			= @HeroId;
+
+	UPDATE dbo.HeroTranslation 
+	SET
+		FirstName	= @FirstName,
+		LastName	= @LastName,
+		MiddleName	= ISNULL(@MiddleName, MiddleName),
+		Biography	= ISNULL(@Biography, Biography),
+		UpdatedBy	= @UpdatedBy,
+		MilitaryGroupId	= ISNULL(@MilitaryGroupId, MilitaryGroupId),
+		MilitaryRankId	= ISNULL(@MilitaryRankId, MilitaryRankId),
+		MilitaryAwardId	= ISNULL(@MilitaryAwardId, MilitaryAwardId)
+	WHERE 
+		HeroId		= @HeroId
+	AND LanguageId	= @LanguageId;
+END;
+
+y
