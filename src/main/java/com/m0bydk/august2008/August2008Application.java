05a69752c3053bcd880d34b0f9fabb3e72c36ba5
David Mumladze <davidlars99@gmail.com>
2021-03-22 22:54:22 -0400

    initial setup

--- src/main/java/com/m0bydk/august2008/August2008Application.java
+++ src/main/java/com/m0bydk/august2008/August2008Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class August2008Application {
 
 	public static void main(String[] args) {
+		//bootstrapper
 		SpringApplication.run(August2008Application.class, args);
 	}
 
